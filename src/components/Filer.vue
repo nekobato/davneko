f01b7eb9af4d7c6679f1d9eb6862cf4eb88528c0
nekobato <nekobato@gmail.com>
2016-09-23 21:33:01 +0900

    add file search input, close #18

--- src/components/Filer.vue
+++ src/components/Filer.vue
@@ -2,17 +2,25 @@
 div.filer(:class='isShow')
   div.card.blue-grey.darken-2.white-text
     div.card-content
-      span.title(@click='toggleNav') davneko
+      span.title davneko
     div.card-action
-      div.btn.file-trigger(@click='addDir2Queue')
+      div.input-field
+        input#search_input.search-input(type='text', v-model='searchText')
+        label.search-label(for='search_input', v-show='!searchText')
+          i.material-icons search
+      div.btn.right.action-btn.col.s2(@click='addDir2Queue')
         i.material-icons playlist_add
   breadcrumbs
   ul.collection.filelist(v-el:filelist-box)
-    li.collection-item.grey-text.text-darken-1.file-item(v-for='file in filelist', @click='selectFile(file)')
+    li.collection-item.grey-text.text-darken-1.file-item(
+      v-for='file in filelist | filelistFilter',
+      track-by="$index"
+      @click='selectFile(file)')
       //- i.material-icons {{ file | file2IconName }}
       span.truncate {{file.name}}
 </template>
 <script>
+import _ from 'lodash'
 import Breadcrumbs from './BreadCrumbs.vue'
 import { fetchDir, addDir2Queue, selectFile, ressurectDepth } from '../vuex/actions'
 
@@ -37,14 +45,21 @@ export default {
   components: {
     Breadcrumbs
   },
-  data() {
+  data () {
     return {
-      nav: {
-        isVisible: true
-      }
+      searchText: ''
     }
   },
   filters: {
+    filelistFilter (filelist) {
+      if (this.$data.searchText) {
+        return _.filter(filelist, (file) => {
+          const re = new RegExp(this.$data.searchText, 'g')
+          return re.test(file.name)
+        })
+      }
+      return this.filelist
+    },
     file2IconName(file) {
       if (/\.(ogg|wav|mp3|aac|m4a)$/.test(file.name)) {
         return "file_audio"
@@ -52,11 +67,6 @@ export default {
       if (file.type === "directory") {
         return "folder"
       }
-    }
-  },
-  methods: {
-    toggleNav() {
-      this.$data.nav.isVisible = !this.$data.nav.isVisible
     }
   },
   created() {
@@ -96,6 +106,21 @@ $keyframes filer-arrival
   li.not-selectable
     &:hover
       background: white
+.action-btn
+  margin: 0 5px 0 0
+  &:last-child
+    margin: 0
+.input-field
+  display: inline-block
+  margin: 0
+  width: 160px
+.search-input
+  margin: 0
+  height: 36px
+.search-label
+  top: 0.5rem
+  left: auto
+  right: 0.25rem
 .title
   font-size: 2em
 .filelist
