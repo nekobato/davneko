9cdea11bc4d9396a8611b6820a991deab88aa85c
nekobato <nekobato@gmail.com>
2019-03-26 17:41:03 +0900

    update: frontend module dependencies

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
-<template lang="jade">
-div.z-depth-1.davneko
-  filer
-  div.z-depth-2.audiobox(v-el:audiobox)
-    player
-    playlist
-    button.btn.teal.lighten-1.filer-toggle(@click='toggleFiler')
+<template>
+  <div class="z-depth-1 davneko">
+    <filer></filer>
+    <div class="z-depth-2 audiobox" ref="audiobox">
+      <player></player>
+      <playlist></playlist>
+      <button class="btn teal lighten-1 filer-toggle" @click="toggleFiler"></button>
+    </div>
+  </div>
 </template>
 <script>
-import store from './vuex/store'
-import Filer from './components/Filer'
-import Player from './components/Player'
-import Playlist from './components/Playlist'
+import store from "./store";
+import Filer from "./components/Filer.vue";
+import Player from "./components/Player.vue";
+import Playlist from "./components/Playlist.vue";
 
 export default {
-  el: "#davneko",
+  name: "app",
   components: {
     Filer,
     Player,
@@ -21,51 +23,66 @@ export default {
   },
   store,
   methods: {
-    toggleFiler: function () {
-      this.$els.audiobox.classList.toggle('filer-view')
+    toggleFiler: function() {
+      this.$refs.audiobox.classList.toggle("filer-view");
+    }
+  }
+};
+</script>
+<style lang="stylus" scoped>
+$width-pc = 992px;
+$side-nav-width = 50%;
+
+.davneko {
+  position: relative;
+  display: flex;
+  margin: auto;
+  height: 100%;
+  overflow: hidden;
+
+  @media (max-width: $width-pc) {
+    width: 460px;
+  }
+}
+
+.audiobox {
+  position: absolute;
+  left: 460px;
+  right: 0;
+  display: flex;
+  flex-direction: column;
+  margin: auto;
+  width: 460px;
+  height: 100%;
+  z-index: 2;
+  transition: left 0.4s ease 0s;
+
+  @media (max-width: $width-pc) {
+    left: 0;
+
+    &.filer-view {
+      left: 460px;
     }
   }
 }
-</script>
-<style lang="stylus" scoped>
-$width-pc = 992px
-$side-nav-width = 50%
 
-.davneko
-  position: relative
-  display: flex
-  margin: auto
-  height: 100%
-  overflow: hidden
-  @media (max-width: $width-pc)
-    width: 460px
-.audiobox
-  position: absolute
-  left: 460px
-  right: 0
-  display: flex
-  flex-direction: column
-  margin: auto
-  width: 460px
-  height: 100%
-  z-index: 2
-  transition: left 0.4s ease 0s
-  @media (max-width: $width-pc)
-    left: 0
-    &.filer-view
-      left: 460px
-.filer-toggle
-  position: absolute
-  top: 80px
-  left: -40px
-  z-index: 2
-  margin: auto
-  padding: 0
-  width: 80px
-  height: 40px
-  border-radius: 5px
-  .material-icons
-    font-size: 2rem
-  @media (min-width: $width-pc)
-    display: none
+.filer-toggle {
+  position: absolute;
+  top: 80px;
+  left: -40px;
+  z-index: 2;
+  margin: auto;
+  padding: 0;
+  width: 80px;
+  height: 40px;
+  border-radius: 5px;
+
+  .material-icons {
+    font-size: 2rem;
+  }
+
+  @media (min-width: $width-pc) {
+    display: none;
+  }
+}
 </style>
---
+++ src/store/mutation-types.js
@@ -0,0 +1,36 @@
+export const START_FETCH_DIR = 'START_FETCH_DIR';
+export const FETCH_DIR = 'FETCH_DIR';
+export const RECEIVE_DIR = 'RECEIVE_DIR';
+
+export const INIT_DEPTH = 'INIT_DEPTH';
+export const ADD_DEPTH = 'ADD_DEPTH';
+export const UPDATE_DEPTH = 'UPDATE_DEPTH';
+
+export const ADD_QUEUE = 'ADD_QUEUE';
+export const ADD_QUEUES = 'ADD_QUEUES';
+export const REMOVE_QUEUE = 'REMOVE_QUEUE';
+export const REMOVE_QUEUES = 'REMOVE_QUEUES';
+export const UPDATE_QUEUES = 'UPDATE_QUEUES';
+
+export const PLAY_QUEUE = 'PLAY_QUEUE';
+export const PLAY_NEXT = 'PLAY_NEXT';
+export const PLAY_PREV = 'PLAY_PREV';
+
+export const CHANGE_LOOP = 'CHANGE_LOOP';
+
+export const UPDATE_TIME = 'UPDATE_TIME';
+
+export const AUDIO_PLAYED = 'AUDIO_PLAYED';
+export const AUDIO_PAUSED = 'AUDIO_PAUSED';
+export const AUDIO_PREV = 'AUDIO_PREV';
+export const AUDIO_NEXT = 'AUDIO_NEXT';
+export const AUDIO_TIME_UPDATED = 'AUDIO_TIME_UPDATED';
+export const AUDIO_ENDED = 'AUDIO_ENDED';
+export const AUDIO_TOGGLE_MUTE = 'AUDIO_TOGGLE_MUTE';
+export const AUDIO_VOLUME_UP = 'AUDIO_VOLUME_UP';
+export const AUDIO_VOLUME_DOWN = 'AUDIO_VOLUME_DOWN';
+
+export const PLAYER_PLAYED = 'PLAYER_PLAYED';
+export const PLAYER_PAUSED = 'PLAYER_PAUSED';
+export const PLAYER_ENDED = 'PLAYER_ENDED';
+export const REMOVE_QUEUES_ALL = 'REMOVE_QUEUES_ALL';
