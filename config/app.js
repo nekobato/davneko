7b8baf4e2ac9da6c01b7d39ae63c905321313094
nekobato <nekobato@gmail.com>
2020-08-10 01:51:56 +0900

    migrate: pug to html

--- config/app.js
+++ config/app.js
@@ -1,13 +1,13 @@
-const bodyParser = require('body-parser');
-const express = require('express');
-const passport = require('passport');
-const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
-const path = require('path');
-const cors = require('cors');
-const app = express();
+const bodyParser = require("body-parser")
+const express = require("express")
+const passport = require("passport")
+const session = require("express-session")
+const MongoStore = require("connect-mongo")(session)
+const path = require("path")
+const cors = require("cors")
+const app = express()
 
-const config = require('./config');
+const config = require("./config")
 
 app.use(
   session({
@@ -15,65 +15,63 @@ app.use(
     resave: true,
     saveUninitialized: true,
     store: new MongoStore({
-      url: 'mongodb://root@localhost/davneko_session',
+      url: "mongodb://root@localhost/davneko_session",
     }),
     cookie: {
       httpOnly: false,
     },
   })
-);
+)
 
-app.use(cors());
+app.use(cors())
 
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(passport.initialize())
+app.use(passport.session())
 
 // app set
-app.set('views', path.join(__dirname, '../views'));
-app.set('view engine', 'pug');
-if (app.get('env') === 'development') {
-  app.set('view options', { pretty: true });
+app.set("views", path.join(__dirname, "../views"))
+app.set("view engine", "html")
+if (app.get("env") === "development") {
+  app.set("view options", { pretty: true })
 }
 
 // app use
 // app.use require("serve-favicon")('./public/favicon.ico')
-app.use(require('morgan')('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(require('cookie-parser')());
-app.use('/', require('./routes'));
-app.use(express.static(path.join(__dirname, '../dist')));
-app.post('/auth', require('./auth'));
+app.use(require("morgan")("dev"))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(require("cookie-parser")())
+app.use("/", require("./routes"))
+app.use(express.static(path.join(__dirname, "../dist")))
+app.post("/auth", require("./auth"))
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+  const err = new Error("Not Found")
+  err.status = 404
+  next(err)
+})
 
 // error handlers
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res) {
-    res.status(err.status || 500);
-    res.render('error', {
+if (app.get("env") === "development") {
+  app.use((err, req, res) => {
+    res.status(err.status || 500).json({
       message: err.message,
       error: err,
-    });
-  });
+    })
+  })
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res) {
-  res.status(err.status || 500);
-  res.render('error', {
+app.use((err, req, res) => {
+  res.status(err.status || 500).json({
     message: err.message,
     error: {},
-  });
-});
+  })
+})
 
-module.exports = app;
+module.exports = app
