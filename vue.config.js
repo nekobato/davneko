77ef155cc3b2cc439e12f53b7b183c7d1d9e3dfc
nekobato <nekobato@gmail.com>
2020-08-10 01:58:20 +0900

    add: vue config

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  chainWebpack: config => {
+    const svgRule = config.module.rule("svg")
+
+    svgRule.uses.clear()
+
+    svgRule.use("vue-svg-loader").loader("vue-svg-loader")
+  },
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+      },
+    },
+  },
+}
